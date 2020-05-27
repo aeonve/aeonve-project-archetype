e62a6b064cd3a199c62e0c125651490975d1ddec
yangtao <tao.yang01@weimob.com>
2020-05-27 23:58:39 +0800

    优化：编译支持单元测试

--- aeonve-project-archetype-service/src/test/java/com/aeonve/DemoTest.java
+++ aeonve-project-archetype-service/src/test/java/com/aeonve/DemoTest.java
@@ -1,5 +1,12 @@
 package com.aeonve;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +25,24 @@ public class DemoTest {
   @Test
   public void log() {
     LOGGER.info("this is a log test.");
+  }
+
+  @Test
+  public void demo() {
+    File customBundleFile = new File(
+        System.getProperty("user.dir") + "/../settings/properties/project-info-custom-zh_CN.properties");
+    if (customBundleFile.isFile() && customBundleFile.getName().endsWith(".properties")) {
+      String bundleName =
+          customBundleFile.getName().substring(0, customBundleFile.getName().indexOf(".properties"));
+      URLClassLoader classLoader = null;
+      try {
+        classLoader = new URLClassLoader(new URL[]{customBundleFile.getParentFile().toURI().toURL()});
+      } catch (MalformedURLException e) {
+        // could not happen.
+      }
+
+      ResourceBundle bundle = ResourceBundle.getBundle(bundleName, Locale.getDefault(), classLoader);
+    }
   }
 
 }
